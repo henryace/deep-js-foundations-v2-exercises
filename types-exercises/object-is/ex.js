be983fb4e4820277d1f52782959cd270aca8e05c
grtert <grtert@gmail.com>
2019-09-01 13:15:47 +0800

    Update ex.js

--- types-exercises/object-is/ex.js
+++ types-exercises/object-is/ex.js
@@ -1,4 +1,46 @@
 // TODO: define polyfill for `Object.is(..)`
+// if(!Object.is || true) {
+// 	Object.is = function ObjectIs(x,y) {
+// 	var xNegZero = isItNegZero(x);
+// 	var yNegZero = isItNegZero(y);
+
+// 	if (x === y) {
+// 		return true;
+// 	}
+
+// 	return false;
+
+// };
+// }
+
+if(!Object.is || true) {
+		Object.is = function ObjectIs(x,y) {
+		var xNegZero = isItNegZero(x);
+		var yNegZero = isItNegZero(y);
+
+		if (xNegZero || yNegZero) {
+			return xNegZero && yNegZero;
+		}
+		else if (isItNaN(x) && isItNaN(y)) {
+			return true;
+		}
+		else if (x === y) {
+			return true;
+		}
+
+		return false;
+
+		// **********
+
+		function isItNegZero(x) {
+			return x === 0 && (1 / x) === -Infinity;
+		}
+
+		function isItNaN(x) {
+			return x !== x;
+		}
+	};
+}
 
 
 
@@ -8,12 +50,12 @@ console.log(Object.is("foo","foo") === true);
 console.log(Object.is(false,false) === true);
 console.log(Object.is(null,null) === true);
 console.log(Object.is(undefined,undefined) === true);
-console.log(Object.is(NaN,NaN) === true);
+console.log(Object.is(NaN,NaN) === true); // problems
 console.log(Object.is(-0,-0) === true);
 console.log(Object.is(0,0) === true);
 
-console.log(Object.is(-0,0) === false);
-console.log(Object.is(0,-0) === false);
+console.log(Object.is(-0,0) === false);  // problems
+console.log(Object.is(0,-0) === false);  // problems
 console.log(Object.is(0,NaN) === false);
 console.log(Object.is(NaN,0) === false);
 console.log(Object.is(42,"42") === false);
